12d8fcbe8f565007b945f8f8ca0590980b04b6e8
gishyd <shaqhyd@gmail.com>
2017-03-16 00:47:01 -0500

    added webpack to bundle code

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -2,9 +2,18 @@
 import express from "express";
 import path from "path";
 import open from "open";
+import webpack from "webpack";
+import webpackconfig from "../webpack.config.dev";
+
 
 const port=3000;
 const app= express();
+const compiler = webpack(webpackconfig);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+noInfo: true,
+publicPath: webpackconfig.output.publicPath
+}));
 
 app.get("/",function(req,res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './index.css';
+
+import numeral from "numeral";
+
+const someValue = numeral(1000).format('$0,0.00');
+debugger;
+console.log(`i would par ${someValue} for this course`);
